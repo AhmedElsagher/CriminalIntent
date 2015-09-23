03e5a00de337671047c8cf4af91b77840a2c31e0
AhmedElsagher <ahmed0elsagher@gmail.com>
2015-09-23 09:21:29 +0200

    pager adapter +ViewPager + delete the Crime Activity

--- app/src/main/java/com/example/ahmed/criminalintent/Crime.java
+++ app/src/main/java/com/example/ahmed/criminalintent/Crime.java
@@ -1,12 +1,13 @@
 package com.example.ahmed.criminalintent;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by ahmed on 15/06/15.
  */
-public class Crime {
+public class Crime implements Serializable{
     private UUID mId;
     private String mTitle;
     private Date mDate;
